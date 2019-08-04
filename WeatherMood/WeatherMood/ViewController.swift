6ba50067bb12995cd6568f6a4c01ddeba7be87ee
JonKopp99 <jonkopp99@gmail.com>
2019-08-04 16:32:37 -0700

    First Commit

---
+++ WeatherMood/WeatherMood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMood
+//
+//  Created by Jonathan Kopp on 8/4/19.
+//  Copyright © 2019 Jonathan Kopp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
